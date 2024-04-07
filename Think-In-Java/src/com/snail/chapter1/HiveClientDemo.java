3f78679d8db87d678f74a0122bb0bfdaee671502
taixianchao <taixianchao@miao.cn>
2024-04-07 16:53:56 +0800

    提交

---
+++ Think-In-Java/src/com/snail/chapter1/HiveClientDemo.java
@@ -0,0 +1,13 @@
+package com.snail.chapter1;
+
+/**
+ * @Author Administrator
+ * @Date 2024/4/7 15:24
+ * @Version 1.0
+ * Desc:
+ */
+public class HiveClientDemo {
+    public static void main(String[] args) {
+
+    }
+}
